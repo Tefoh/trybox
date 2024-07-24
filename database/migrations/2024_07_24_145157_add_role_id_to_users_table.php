8ff63801b9c9e038ece64f2f1ce5f8d65ec28faf
Tefoh <tefo.ha27@gmail.com>
2024-07-24 18:38:27 +0330

    feat(users): add role_id column to users table

---
+++ database/migrations/2024_07_24_145157_add_role_id_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Core\Enums\RoleEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Role::class)
+                ->default(RoleEnum::CUSTOMER->value)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeignIdFor(\App\Models\Role::class);
+        });
+    }
+};
