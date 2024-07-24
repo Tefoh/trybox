785fc7565387bd9348ea8ba4865a2542f45075f9
Tefoh <tefo.ha27@gmail.com>
2024-07-24 23:28:22 +0330

    feat(products): implement the queries in product repository

--- Infra/app/Repositories/ProductRepository.php
+++ Infra/app/Repositories/ProductRepository.php
@@ -2,9 +2,13 @@
 
 namespace App\Repositories;
 
+use App\Models\CustomerAddress;
 use App\Models\Product;
 use App\Models\Store;
 use Core\Repositories\ProductRepositoryInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductRepository implements ProductRepositoryInterface
 {
@@ -19,5 +23,57 @@ class ProductRepository implements ProductRepositoryInterface
         $store->products()->sync([$product->id]);
 
         return $product->load('stores');
+    }
+
+    public function getListOfCustomerProducts(int $userId)
+    {
+        $customerAddress = CustomerAddress::query()
+            ->where('user_id', $userId)
+            ->first();
+        if (! $customerAddress) {
+            throw new NotFoundHttpException();
+        }
+
+        $nearbyStores = Store::query()
+            ->select(
+                DB::raw(
+                    'SELECT id,
+                ( 6371 * acos( cos( radians(:c_lat) )
+                    * cos( radians( lat ) )
+                    * cos( radians( long ) - radians(:c_lon) )
+                    + sin( radians(:c_lat) )
+                    * sin( radians( lat ) ) ) ) AS distance
+                FROM locations
+                HAVING distance < :distance
+                ORDER BY distance;'
+                ),
+            [
+                'c_lat' => $customerAddress->lat,
+                'c_lon' => $customerAddress->long,
+                'distance' => 10,
+            ])
+            ->get()
+            ->pluck('id');
+
+        return Product::query()
+            ->where(
+                'store', fn (Builder $q) => $q->whereIn('id', $nearbyStores)
+            )
+            ->paginate();
+    }
+
+    public function getListOfSellerProducts(int $userId)
+    {
+        return Product::query()
+            ->whereHas(
+                'store', fn (Builder $q) => $q->where('user_id', $userId)
+            )
+            ->paginate();
+    }
+
+    public function getListOfProducts()
+    {
+        return Product::query()
+            ->paginate();
     }
 }
