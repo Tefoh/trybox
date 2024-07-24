ef9c6b72c4a768a0496c90200ea37b0ce5ccd820
Tefoh <tefo.ha27@gmail.com>
2024-07-25 00:51:23 +0330

    feat(product): add job for call webhook in background

---
+++ Infra/app/Jobs/ProcessOrderJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Core\Services\Product\ProductService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class ProcessOrderJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        protected readonly array $transaction,
+        protected readonly string $transactionNumber,
+    )
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Http::get(
+            route('webhook.product.buy'),
+            [
+                'transaction_num' => $this->transactionNumber,
+            ]
+        );
+    }
+}
