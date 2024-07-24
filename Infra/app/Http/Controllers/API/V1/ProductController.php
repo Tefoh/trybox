3dd4793d57df1d65cb431657af79700f41b30518
Tefoh <tefo.ha27@gmail.com>
2024-07-25 00:52:14 +0330

    feat(product): route buy products to core

--- Infra/app/Http/Controllers/API/V1/ProductController.php
+++ Infra/app/Http/Controllers/API/V1/ProductController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\BuyProductRequest;
 use App\Http\Requests\API\V1\ProductRequest;
+use App\Jobs\ProcessOrderJob;
+use App\Models\Product;
 use App\Models\Store;
 use Core\Services\Product\ProductService;
+use Core\ValueObjects\Products\BuyProductObject;
 use Core\ValueObjects\Store\AddProductToStoreObject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ProductController extends Controller
 {
@@ -27,6 +32,27 @@ class ProductController extends Controller
 
         return $this->productService->save(
             $productObject, $request->user(), $store->id
+        );
+    }
+
+    public function buy(BuyProductRequest $request, Product $product)
+    {
+        $buyProductObject = new BuyProductObject($request->validated());
+
+        $transaction = $this->productService->buyProduct(
+            $buyProductObject, $request->user(), $product->id
+        );
+
+        Http::fake([
+            '*' => Http::response(['transaction_num' => uniqid()], 200),
+        ]);
+
+        $response = Http::post('http://random.com/anywhere');
+        $transactionNumber = $response->json()['transaction_num'];
+        ProcessOrderJob::dispatch($transaction, $transactionNumber);
+
+        return $this->productService->updateTheTransactionNumber(
+            $transaction, $transactionNumber
         );
     }
 }
--- Infra/routes/api.php
+++ Infra/routes/api.php
@@ -12,4 +12,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/store/{store}/product', [\App\Http\Controllers\API\V1\ProductController::class, 'store'])->name('store.add-product');
 
     Route::get('products', [\App\Http\Controllers\API\V1\ProductController::class, 'index'])->name('product.index');
+
+    Route::post('/products/{product}/buy', [\App\Http\Controllers\API\V1\ProductController::class, 'buy'])->name('products-product.buy');
+
+    Route::get('/webhook/product/buy', [\App\Http\Controllers\API\V1\ProductController::class])->name('webhook.product.buy');
 });
